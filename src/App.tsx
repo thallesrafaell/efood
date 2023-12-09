2b25940491687b36bd9c26e5330c421dc3853c46
Thalles Rafael <thallesrafaell@outlook.com>
2023-12-09 00:41:13 -0300

    Iniciando Criaçao do Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalCSS } from './styles'
 import Rotas from './route'
+import Footer from './components/Footer'
 function App() {
   return (
     <BrowserRouter>
       <GlobalCSS />
       <div>
         <Rotas />
+        <Footer />
       </div>
     </BrowserRouter>
   )
